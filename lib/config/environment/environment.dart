e39ad03066081f13d662d9cfbc8f3cc7dde80234
Swann HERRERA <swann@devloup.dev>
2022-01-29 21:52:21 +0100

    update starter

--- lib/config/environment/environment.dart
+++ lib/config/environment/environment.dart
@@ -1,1 +1,1 @@
-class Environment {}
+abstract class Environment {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'presentation/pages/my_home_page.dart';
+import './ui/screens/home/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Home(),
     );
   }
 }
